e6b04f0a87390dc1b791efd3411f3a481e903d9e
DaffTzy5912 <dafanation999@gmail.com>
2025-05-13 00:09:40 +0700

    Delete public directory

--- public/js/auth.js
+++
@@ -1,9 +0,0 @@
-
-function generateNumber() {
-  const num = Math.floor(100000 + Math.random() * 900000);
-  document.getElementById("userNumber").textContent = num;
-  localStorage.setItem("number", num);
-}
-function login() {
-  window.location.href = "home.html";
-}
