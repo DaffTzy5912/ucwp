e2595d5430852298cab32ec8cb0382f05096cb29
DaffTzy5912 <dafanation999@gmail.com>
2025-05-13 00:12:38 +0700

    Create contact.routes.js

---
+++ routes/contact.routes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const { readDB, writeDB } = require("../db.utils");
+
+// Tambah kontak
+router.post("/", (req, res) => {
+    const { userId, contactName, contactNumber } = req.body;
+    const db = readDB();
+
+    const contactUser = db.users.find(u => u.number === contactNumber);
+    if (!contactUser) return res.status(404).json({ error: "Kontak tidak ditemukan" });
+
+    if (!db.contacts[userId]) db.contacts[userId] = [];
+    db.contacts[userId].push({
+        id: contactUser.id,
+        name: contactName,
+        number: contactNumber
+    });
+
+    writeDB(db);
+    res.json(contactUser);
+});
+
+// Dapatkan daftar kontak
+router.get("/:userId", (req, res) => {
+    const { userId } = req.params;
+    const db = readDB();
+    const contacts = db.contacts[userId] || [];
+    res.json(contacts);
+});
+
+module.exports = router;
